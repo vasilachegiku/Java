13de223756c6f8bd6aa10026a41d2da3184f35ae
vasilachegiku <vasilache.giku7@gmail.com>
2020-09-28 10:57:44 +0300

    Update SelectiveIteration.java

--- Tema pentru acasa/Constructii Cecizionale, Cicluri/#6.1/SelectiveIteration.java
+++ Tema pentru acasa/Constructii Cecizionale, Cicluri/#6.1/SelectiveIteration.java
@@ -3,8 +3,8 @@ public class SelectiveIteration{
 		int num = 1500;
 		
 	for ( int i=0 ; i<1; i++){
-		for (int j=0; j<1500; j++){
-			while(num!=num/3 & num!=(num+5)/7 ){
+		for (int j=0; j<num; j++){
+			while(j!=num/3 & j!=(num+5)/7 ){
 				continue;
 			}
 			System.out.println(j);
